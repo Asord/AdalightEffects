feaad38220b762e2d215e8c65090f31efab37f62
Killian <killian.perche@outlook.fr>
2019-05-01 18:08:15 +0200

    Minor "typedef" modifications for Windows API compatibility (WARNING ! May be replaced by a custom typedefs in the future)

--- AsColor/AsMath.h
+++ AsColor/AsMath.h
@@ -3,20 +3,30 @@
 #include <random>
 
 typedef unsigned char UINT8;
+typedef unsigned char& UINT8R;
 typedef unsigned char* PUINT8;
 typedef const unsigned char* CPUINT8;
+
 typedef unsigned int UINT;
 typedef const unsigned int CUINT;
 typedef unsigned int& UINTR;
 typedef const unsigned int& CUINTR;
+
 typedef float FLOAT;
+typedef const float CFLOAT;
 typedef float& FLOATR;
 typedef float* PFLOAT;
-typedef bool BOOL;
-typedef void VOID;
+
+typedef int BOOL;
+typedef bool _SBOOL;
+
+#ifndef VOID
+#define VOID void
+#endif
 
 namespace Asemco
 {
+
 	#define randRange(n_min, n_max) rand() % (n_max - n_min + 1) + n_min
 
 	VOID normalize(FLOATR num1, FLOATR num2);
@@ -26,4 +36,5 @@ namespace Asemco
 	FLOAT f3max(FLOAT f1, FLOAT f2, FLOAT f3);
 
 	FLOAT ufmodf(FLOAT fnum, FLOAT fmod);
+
 }
