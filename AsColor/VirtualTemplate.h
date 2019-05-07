24547f202839c913b25c7d13e78145f882589b17
Killian <killian.perche@outlook.fr>
2019-05-07 21:16:55 +0200

    The virtual template base use a Controller template to communicate with any devices within the Update function (to place in a loop)

---
+++ AsColor/VirtualTemplate.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "SerialController.h"
+
+namespace Asemco
+{
+	class VirtualTemplate
+	{
+	protected:
+		UINT effectID;
+		Controller* controller;
+
+	public:
+		VirtualTemplate(UINT effectID, Controller* controller) 
+		{ 
+			this->effectID = effectID;
+			this->controller = controller; 
+		};
+
+		virtual VOID Update() = 0;
+	};
+}
